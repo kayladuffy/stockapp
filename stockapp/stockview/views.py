21abd1ed7dec3c58a3bc3240b083487d713a8ba5
Kayla.duffy <kayla.duffy@whag18kayla.lan>
2020-10-07 21:04:13 -0500

    Tweaked button and text box verbiage

--- stockapp/stockview/views.py
+++ stockapp/stockview/views.py
@@ -30,7 +30,7 @@ def add_stock(request):
 
         if form.is_valid():
             form.save()
-            message.success(request, ("Stock has been added."))
+            messages.success(request, ("Success! Stock has been added."))
             return redirect('add_stock')
     else: 
         ticker = stocks.objects.all()
