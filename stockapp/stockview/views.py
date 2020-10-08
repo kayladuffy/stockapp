8857f1c06adbc072793399a54e016469a62e754a
Kayla.duffy <kayla.duffy@whag18kayla.lan>
2020-10-07 20:16:58 -0500

    Created ticker to enable variable search

--- stockapp/stockview/views.py
+++ stockapp/stockview/views.py
@@ -3,16 +3,20 @@ from django.shortcuts import render
 def home(request):
     import requests 
     import json
-    #pk_ea4736199eb44c189f914c2ed20cade4
-    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_ea4736199eb44c189f914c2ed20cade4")
+
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+        api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_ea4736199eb44c189f914c2ed20cade4")
+
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error"
+        return render(request, 'home.html', {'api': api})
     
+    else:
+        return render(request, 'home.html', {'ticker': "Enter symbol"})
 
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "Error"
-
-    return render(request, 'home.html', {'api': api})
 
 def about(request):
     return render(request, 'about.html', {})
