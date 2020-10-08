76d8ebff496a7f17c79c1c12f2cfb0a3f2d38137
Kayla.duffy <kayla.duffy@whag18kayla.lan>
2020-10-07 19:29:26 -0500

    installed requests and added api key

--- stockapp/stockview/urls.py
+++ stockapp/stockview/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('about.html', views.about, name="about us"),
+    path('about.html', views.about, name="about"),
 ]
